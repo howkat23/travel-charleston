bcab791ea16a456f3d7fd303efc8a5f6efed1ee8
howkat23 <howardkaterina20@gmail.com>
2023-08-05 19:04:57 -0400

    your commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+const [attraction, setTodo] = useState(data);
+const [showMore, setShowMore] = useState(false);
+
+
+const removeTodo = (id) => {
+  let newTodo = attraction.filter((todo) => todo.id !== id);
+  setTodo(newTodo)
+}
+  return (<div>
+      <div className="container">
+        <h1>Top Attractions in Charleston</h1>
+      </div>
+{attraction.map((todo => {
+      const { id, todoName, description, image } = todo;
+return(
+      <div key={id}>
+        <div className='container'>
+          <h2>{id} - {todoName}</h2>
+        </div>
+
+        <div className='container'>
+          <p>{showMore ? description : description.substring(0,220) + "...."}
+          <button onClick={() => setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"}</button>
+          </p>
+        </div>
+
+      <div className='container'>
+        <img src={image} width="500px"/>
+      </div>
+
+      <div className='container'>
+
+        </div>
+
+
+        <div className='container'>
+<button className="btn" onClick={() => removeTodo(id)}>Remove</button>
+        </div>
+
+        </div>)
+    }))}
     </div>
-  );
+  )
 }
 
+
+
+
+
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const data = [
+
+    {
+    id: 1,
+    todoName: "Joe Riley Waterfront Park",
+    description: "Overlooking Charleston Harbor and Cooper Park on the eastern coast of the peninsula, this expansive waterfront green space is perfect for laidback strolls and picnics.Life in the Lowcountry is all about slowing down and taking a moment or two to relax, and locals and visitors both agree that Charleston's Waterfront Park is the perfect venue to stretch out and soak up the scene. With a prime location overlooking Charleston Harbor and the Cooper River, and more than 10 acres of room to roam, (most of which is waterfront), Waterfront Pak is essentially a romantic, engaging, serene, and perfectly picturesque destination that will make any newcomer fall in love with this unique southern city.",
+    image: "https://media.istockphoto.com/id/1407292724/photo/charleston-south-carolina-usa-at-waterfront-park.webp?b=1&s=612x612&w=0&k=20&c=2NBAQ5bsLhNBfqcQNY7E-DX4wSMCit85wou_oUvm3iA=",
+    source: "Tripadvisor.com/"
+    },
+
+    {
+    id: 2,
+    todoName: "Middleton Place",
+    description: "Middleton Place National Historic Landmark is home to America’s oldest and most important landscaped gardens. Began in 1741, the historic site today encompasses 110 acres including the Gardens, House Museum, Stableyards, and Eliza’s House. Together they tell the inclusive history of all who lived, worked, and died here. The stories of the Middleton family including two Founding Fathers and generations of enslaved people are interwoven throughout the property. Visitors have opportunities to experience those stories with both guided or self-guided tours.",
+    image: "https://cdn.pixabay.com/photo/2017/01/29/17/36/oak-tree-2018822_1280.jpg",
+    source: "Tripadvisor.com"
+    },
+
+    {
+    id: 3,
+    todoName: "Fort Sumter National Monument",
+    description: "Learn all about this storied spot of the first clash between the Union and Confederate armies in the Civil War, in which the Union forces finally surrendered after a 34-hour bombardment by the Rebels. Two forts stand at the entrance of Charleston Harbor. Patriots inside a palmetto log fort, later named Fort Moultrie, defeated the Royal Navy in 1776. As Charleston blazed a path towards secession to preserve slavery, construction on a new fort, Fort Sumter, proceeded. The Confederacy fired on the US garrison of Fort Sumter on April 12, 1861 opening the Civil War, which redefined American freedom.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvJQtn-DsijMZ1AyUrYByB7MXl-RTLneSgtA&usqp=CAU",
+    source: "Tripadvisor.com"
+
+    }
+
+    ]
